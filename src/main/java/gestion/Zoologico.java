926d4243da90f0a000e3c02ab4e0c4ad06e27830
Camila Rios <mriosm@unal.edu.co>
2023-09-26 08:46:02 -0500

    3

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,5 +1,57 @@
 package gestion;
+import java.util.ArrayList;
 
 public class Zoologico {
 
+	private Sting nombre;
+	private String ubicacion;
+	private ArrayLis<Zona>
+	zonas = new ArrayList<>();
+		
+	public Zoologico() {
+		this(null,null);
+	}
+	
+	public Zoologico(String name, String ubi) {
+		this.nombre = name;
+		this.ubicacion = ubi;
+	}
+	
+	public void agregarZonas(Zona zone) {
+		this.zonas.add(zona);
+		zone.setZoo(this);
+	}
+	
+	public int cantidadTotalAnimales() {
+		int count = 0;
+		for (int i = 0; i < zonas.size(); i++) {
+			count +=
+			this.zonas.get(i).cantidadAnimales();
+		}
+		return count;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	
+	public ArrayList<Zona> getZona(){
+		return zonas;
+	}
+	
+	public void setNombre(String name) {
+		this.nombre = name;
+	}
+	
+	public void setUbicacion(String ubi) {
+		this.ubicacion = ubi;
+	}
+	
+	public void setZona(ArrayList<Zona>zone) {
+		this.zonas = zone;
+	}
 }
